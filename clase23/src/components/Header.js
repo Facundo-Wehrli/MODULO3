4576dc6cac5148aff4fa64e048f157770a1b2331
Facundo Wehrli <wehrlifacundo9@gmail.com>
2022-10-31 12:09:59 -0300

    clase22

---
+++ clase23/src/components/Header.js
@@ -0,0 +1,13 @@
+const Header = (props) => {
+    return (
+        <div>
+            <p>hola soy el Header</p>
+            <p>lorem ñaefisaj ñsaeifjsj ñoisae jsaiñ saeji
+                sfajei sjeañi sjefañi jsiae
+                sjefaiojsañeiosfeajiñsefsjef saejfois
+                sfjñaeisefñjsjeisefñj
+            </p></div>
+
+    )
+}
+export default Header; 
