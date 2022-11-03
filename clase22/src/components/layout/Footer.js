f0c5e547183eb498ec86bd7aee8eb309fd1602b6
Facundo Wehrli <wehrlifacundo9@gmail.com>
2022-11-03 11:45:37 -0300

    ignore

---
+++ clase22/src/components/layout/Footer.js
@@ -0,0 +1,8 @@
+const Header = (props) => {
+    return (
+     <footer>
+     este es el footer
+     </footer>
+    )
+}
+export default Header; 
---
+++ clase22/src/components/layout/Nav.js
@@ -0,0 +1,8 @@
+const Header = (props) => {
+    return (
+     <nav>
+        esta es la navegacion
+     </nav>
+    )
+}
+export default Header; 
