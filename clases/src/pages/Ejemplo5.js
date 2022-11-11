76d6856d4b9ba678acc1ee35926e07d48b039b79
Facundo Wehrli <wehrlifacundo9@gmail.com>
2022-11-11 09:14:02 -0300

    ejemplo5 completado

--- clases/src/pages/Ejemplo5.js
+++ clases/src/pages/Ejemplo5.js
@@ -1,5 +1,5 @@
 import '../style.css/ejemplo5.css';
-import { useEfect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 const Ejemplo5 = props => {
@@ -8,7 +8,7 @@ const Ejemplo5 = props => {
     const [personajes, setPersonajes] = useState([])
 
     /* Fetching data from the API and setting the state of the component. */
-    useEfect(() => {
+    useEffect(() => {
         const cargarDatos = async () => {
             setCargando(true);
             const res = await (await fetch('https://rickandmortyapi.com/api/character')).json();
@@ -22,29 +22,28 @@ const Ejemplo5 = props => {
 
     return (
         <>
-            <h1>Hola soy la página Ejemplo 5 API</h1>
-            {cargando ? <p>cargando datos ...</p>:  
-            <div className="personajes">
-                {personajes.map(personaje => (  
-                <div className="personaje" key={personaje}> 
-                    <h4>{personaje.name}</h4>
-                    <div className="ficha">
-                        <div className="foto">      
-                            <img src={personaje.image} alt={personaje.name} />
-                        </div>
-                        <div className="datos">
-                            <h6>Especie: {personaje.species ==='Human' ? 'Humano' : personaje.species}</h6>
-                            <h6>Vivo: {personaje.status ==='Alive' ? 'Si': 'No'} </h6>
-                        </div>
-                    </div> 
-                   )){'}'}
-                </div>
-    )
-                
-                
 
+            <h1>Hola soy la página Ejemplo 5 API</h1>
+            {cargando ? <p>cargando datos ...</p> :
+                <div className="personajes">
+                    {personajes.map(personaje => (
+                        <div className="personaje" key={personaje}>
+                            <h4>{personaje.name}</h4>
+                            <div className="ficha">
+                                <div className="foto">
+                                    <img src={personaje.image} alt={personaje.name} />
+                                </div>
+                                <div className="datos">
+                                    <h6>Especie: {personaje.species === 'Human' ? 'Humano' : personaje.species}</h6>
+                                    <h6>Vivo: {personaje.status === 'Alive' ? 'Si' : 'No'} </h6>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                }
         </>
-    )
+    )  
 }
 
 export default Ejemplo5;
